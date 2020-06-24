8b8be0311763f8e39bd5cd14f521f3a6ab0e74ac
moer <2453116585@qq.com>
2020-06-24 11:31:52 +0800

    Initial commit

---
+++ js/FuLian_Homepage_js.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+    var d1 = document.getElementById("dom1");
+    d1.onmouseenter = function () {
+        var h1 = document.getElementById("hid1");
+            h1.style.display = 'block'
+        var s1 = document.getElementById("show1");
+            s1.style.display = 'none'
+    }
+    d1.onmouseleave = function () {
+        var s1 = document.getElementById("show1");
+            s1.style.display = 'block'
+        var h1 = document.getElementById("hid1");
+            h1.style.display = 'none'
+    }
+    var d2 = document.getElementById("dom2");
+    d2.onmouseenter = function () {
+        var h2 = document.getElementById("hid2");
+            h2.style.display = 'block'
+        var s2 = document.getElementById("show2");
+            s2.style.display = 'none'
+    }
+    d2.onmouseleave = function () {
+        var s2 = document.getElementById("show2");
+            s2.style.display = 'block'
+        var h2 = document.getElementById("hid2");
+            h2.style.display = 'none'
+    }
+    var d3 = document.getElementById("dom3");
+    d3.onmouseenter = function () {
+        var h3 = document.getElementById("hid3");
+            h3.style.display = 'block'
+        var s3 = document.getElementById("show3");
+            s3.style.display = 'none'
+    }
+    d3.onmouseleave = function () {
+        var s3 = document.getElementById("show3");
+            s3.style.display = 'block'
+        var h3 = document.getElementById("hid3");
+            h3.style.display = 'none'
+    }
+    var d4 = document.getElementById("dom4");
+    d4.onmouseenter = function () {
+        var h4 = document.getElementById("hid4");
+            h4.style.display = 'block'
+        var s4 = document.getElementById("show4");
+            s4.style.display = 'none'
+    }
+    d4.onmouseleave = function () {
+        var s4 = document.getElementById("show4");
+            s4.style.display = 'block'
+        var h4 = document.getElementById("hid4");
+            h4.style.display = 'none'
+    }
+    var d5 = document.getElementById("dom5");
+    d5.onmouseenter = function () {
+        var h5 = document.getElementById("hid5");
+            h5.style.display = 'block'
+        var s5 = document.getElementById("show5");
+            s5.style.display = 'none'
+    }
+    d5.onmouseleave = function () {
+        var s5 = document.getElementById("show5");
+            s5.style.display = 'block'
+        var h5 = document.getElementById("hid5");
+            h5.style.display = 'none'
+    }
+}
